9dfa096567d4e59e96f5ec9641c76e228a0e1216
Subramanian Ramajayam <maniramramajayam@gmail.com>
2025-11-05 02:20:36 +0530

    Add files via upload

--- stlitchem.py
+++ stlitchem.py
@@ -15,6 +15,8 @@ st.write("Click a button below to open an app (make sure each app is running on 
 # Render buttons for each app
 for app_label, url in apps.items():
     if st.button(f"Launch {app_label}"):
-        st.markdown(f'<meta http-equiv="refresh" content="0; url={url}">', unsafe_allow_html=True)
+       st.link_button(f"Launch {app_label}", url)
+ 
+#st.markdown(f'<meta http -equiv="refresh" content="0; url={url}">', unsafe_allow_html=True)
 
 st.info("Developed by Subramanian Ramajayam")
