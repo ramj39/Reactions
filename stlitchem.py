7e56f36a80f639d77956d610ffc1ef77c81a6b9a
Subramanian Ramajayam <maniramramajayam@gmail.com>
2025-11-04 21:20:17 +0530

    Add files via upload

---
+++ stlitchem.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.set_page_config(page_title="Chemical Reactions Dashboard", layout="centered")
+st.title("💊 Chemical Dashboard")
+
+st.title("💊 Chemical_ Dashboard")
+# Dictionary with display names and their respective URLs
+apps = {
+    "pages/streamlit_chemical_reactions.py": "http://localhost:8501",
+    "pages/Streamlit_organic_rxns.py": "http://localhost:8502",
+    #"Audio Module": "http://localhost:8503"
+}
+
+st.write("Click a button below to open an app (make sure each app is running on its own port):")
+
+# Render buttons for each app
+for app_label, url in apps.items():
+    if st.button(f"Launch {app_label}"):
+        st.markdown(f'<meta http-equiv="refresh" content="0; url={url}">', unsafe_allow_html=True)
+
+st.info("Developed by Subramanian Ramajayam")
