f0269e3c8b787c4f0a8928cc78ae935e0e2b29c2
Subramanian Ramajayam <maniramramajayam@gmail.com>
2025-11-04 22:06:28 +0530

    Add files via upload

--- stlitchem.py
+++ stlitchem.py
@@ -5,9 +5,12 @@ st.title("💊 Chemical Dashboard")
 st.title("💊 Chemical_ Dashboard")
 # Dictionary with display names and their respective URLs
 apps = {
-    "pages/streamlit_chemical_reactions.py": "http://localhost:8501",
-    "pages/Streamlit_organic_rxns.py": "http://localhost:8502",
-    #"Audio Module": "http://localhost:8503"
+    #"pages/streamlit_chemical_reactions.py": "http://localhost:8501",
+    #"pages/Streamlit_organic_rxns.py": "http://localhost:8502",
+    "pages/Reactions/streamlit_chemical_reactions.py at main.ramj39/Reactions"
+    "pages/Reactions/streamlit_organic_rxns.py at main.ramj39/Reactions",
+
+    
 }
 
 st.write("Click a button below to open an app (make sure each app is running on its own port):")
